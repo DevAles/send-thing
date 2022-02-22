dcc56052298dcce0746a21077cc1d4a22bd83517
DevAles <devalesoficial@gmail.com>
2022-02-22 16:45:35 -0300

    Install socket.io

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const express = require('express');
+import http from 'http';
+import express from 'express';
+import { Server } from "socket.io";
+
 const app = express();
+const server = http.createServer(app);
+const sockets = new Server(server);
 
 app.use(express.static(`public`));
 app.listen(8080, function(){
     console.log('Server listening on port 8080');
+})
+
+sockets.on('connection', function(socket){
+    const user = socket.id;
+
+    console.log('Connected to socket with user: ' + user)
 })
