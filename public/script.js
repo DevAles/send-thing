9df580ae4e102d4b507fc0b3bf7419b5e9e007f3
DevAles <devalesoficial@gmail.com>
2022-02-25 18:51:31 -0300

    Implement Factory in script module

---
+++ public/script.js
@@ -0,0 +1,36 @@
+export default function script(socket){
+
+    function displayMessage(messageData, userId){
+        const messageDisplayer = document.getElementById('message-displayer')
+        const defineClass = messageData.userId == userId ? 
+        'class="my-message-div"': 'class="other-message-div"';
+    
+        const messageHTML = 
+        `
+        <div ${defineClass}>
+            <p class="message-text">${messageData.message}</p>
+            <p class="message-info">${messageData.username}</p>
+        </div>
+        `
+        messageDisplayer.innerHTML += messageHTML
+        messageDisplayer.scroll(10000000, 10000000)
+    }
+
+    function sendMessage(username, message){
+        const userId = socket.id
+        
+        const messageData = {
+            username,
+            message,
+            userId
+        }
+
+        window.document.getElementById('message-input').value = ''
+        console.log(`${messageData.username}: send message to server`)
+        socket.emit('message', messageData)
+    }
+    return{
+        displayMessage,
+        sendMessage
+    }
+}
